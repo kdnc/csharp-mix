3df16cb239f9e6c065aafee91f4d63c3ab13bf8b
kdnc <nuwanlanka@gmail.com>
2016-08-25 16:05:33 +0530

    Operator overloading

---
+++ csharp-language/operator-overloading/OperatorOverloading/01-operator-overloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(3, 1);
+            Complex c2 = new Complex(1, 2);
+
+            Console.WriteLine("c1 == c2: {0}", c1 == c2);
+            Console.WriteLine("c1 != c2: {0}", c1 != c2);
+            Console.WriteLine("c1 + c2 = {0}", c1 + c2);
+            Console.WriteLine("c1 - c2 = {0}", c1 - c2);
+            Console.WriteLine("c1 * c2 = {0}", c1 * c2);
+            Console.WriteLine("c1 / c2 = {0}", c1 / c2);
+
+            Console.ReadKey();
+        }
+    }
+}
