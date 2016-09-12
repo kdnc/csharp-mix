9960aa04ba13fe2f1c25a408335550e0c63a82e0
kdnc <nuwanlanka@gmail.com>
2016-09-12 13:00:51 +0530

    Asynchronous Programming

---
+++ csharp-language/threading/AynchronousProgramming/02-using-async-await/AsyncAwaitDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class AsyncAwaitDemo
+    {
+        public async Task DoStuff()
+        {
+//            Launches a long running operation called LongRunningOperation()
+            await Task.Run(() =>
+            {
+                LongRunningOperation();
+            });
+        }
+
+        private static async Task<string> LongRunningOperation()
+        {
+            int counter;
+
+            for (counter = 0; counter < 50000; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+
+            return "Counter = " + counter;
+        }
+    }
+}
