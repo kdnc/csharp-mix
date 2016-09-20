827eaf16948400ae33c96c7de169b9ec6215ed04
Nuwan Chamara <nuwanlanka@gmail.com>
2016-09-20 20:35:14 +0530

    Asynchronous Programming

---
+++ csharp-language/threading/AynchronousProgramming/01-creating-a-task-v1-task.start/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            // Create a task and supply a user delegate by using a lambda expression. 
+            Task taskA = new Task(() => Console.WriteLine("Hello from taskA."));
+            // Start the task.
+            taskA.Start();
+
+            // Output a message from the calling thread.
+            Console.WriteLine("Hello from thread '{0}'.",
+                              Thread.CurrentThread.Name);
+            taskA.Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
+// The example displays output like the following:
+//       Hello from thread 'Main'.
+//       Hello from taskA.
---
+++ csharp-language/threading/AynchronousProgramming/02-creating-a-task-v2-task.run/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            // Define and run the task.
+            Task taskA = Task.Run(() => Console.WriteLine("Hello from taskA."));
+
+            // Output a message from the calling thread.
+            Console.WriteLine("Hello from thread '{0}'.",
+                              Thread.CurrentThread.Name);
+            taskA.Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
+// The example displays output like the following:
+//       Hello from thread 'Main'.
+//       Hello from taskA.
