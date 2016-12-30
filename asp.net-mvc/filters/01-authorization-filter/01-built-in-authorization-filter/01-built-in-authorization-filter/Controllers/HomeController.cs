13ac37486e7d71abafb3096050b4052ddedc9651
kdnc <nuwanlanka@gmail.com>
2016-12-30 16:37:51 +0530

    Authorization filter

---
+++ asp.net-mvc/filters/01-authorization-filter/01-built-in-authorization-filter/01-built-in-authorization-filter/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
