a27aa7288cd312e027259114170ae9c3d101a839
kdnc <nuwanlanka@gmail.com>
2016-12-30 16:36:50 +0530

    Asynchronous Programming

---
+++ csharp-language/threading/AynchronousProgramming/03-display-web-page-length-asynchronously/NonAsyncForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace test
+{
+    class NonAsyncForm : Form
+    {
+        Label label;
+        Button button;
+
+        public NonAsyncForm()
+        {
+            label = new Label { Location = new Point(10, 20), Text = "Length" };
+            button = new Button { Location = new Point(10, 50), Text = "Click" };
+            button.Click += DisplayWebSiteLength;
+            AutoSize = true;
+            Controls.Add(label);
+            Controls.Add(button);
+        }
+
+        void DisplayWebSiteLength(object sender, EventArgs e)
+        {
+            label.Text = "Fetching...";
+            using (WebClient client = new WebClient())
+            {
+                string text = client.DownloadString("https://en.wikipedia.org/wiki/United_States");
+                label.Text = text.Length.ToString();
+            }
+        }
+
+        
+    }
+}
---
+++ csharp-language/threading/AynchronousProgramming/03-display-web-page-length-asynchronously/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test;
+
+namespace test
+{
+    static class Program
+    {
+        static void Main()
+        {
+//            Application.Run(new NonAsyncForm());
+            Application.Run(new AsyncForm());
+        }
+    }
+}
