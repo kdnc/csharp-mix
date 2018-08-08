d895571dd042c32d4b35ec70be5338a519d59fe9
Nuwan Chamara <nuwanlanka@gmail.com>
2018-08-08 06:42:26 +0530

    Added migrations

---
+++ entity-framework/querying-and-saving-data/src/Services/App/Kdnc.App.Repository/Migrations/20180403000811_valueobject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Kdnc.App.Repository.Migrations
+{
+    public partial class valueobject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
